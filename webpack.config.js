f7174c483fb31f923269f3cdc37c1cbd4697ad54
arase ito <49282614+araseito@users.noreply.github.com>
2024-08-06 18:58:41 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/consolidatedApp.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
